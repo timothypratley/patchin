81a31eb6c1dc540bb0063ab381dc531815ebe6f1
Timothy Pratley <timothypratley@gmail.com>
2014-10-05 21:27:55 -0700

    removing unnecessary dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject patchin "0.1.0-SNAPSHOT"
+(defproject patchin "0.1.0"
   :description "Creates and applies patches to datastructures"
   :url "http://github.com/patchin"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:dependencies [[criterium "0.4.3"]
+                                  [org.clojure/test.check "0.5.9"]]}})
